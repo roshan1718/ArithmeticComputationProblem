d190ca7b611ec302316e0770b48af6f7e3c9e1fd
roshan1718 <roshanshinde1717@gmail.com>
2020-03-18 19:20:36 +0530

    ADDED code to take user inpute

---
+++ arithmeticComputation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+# @Description : Arithmatic Computation Problem
+# @Author :  Roshan Balkrushna Shinde
+# @Since : 18-03-2020 
+
+read -p "Enter First Number" firstNumber
+read -p "Enter Second Number" secondNumber
+read -p "Enter Third Number" thirdNumber
